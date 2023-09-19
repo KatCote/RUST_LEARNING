7635a92244a747c8bba35819a4646a11f9710928
KatCote <filtermr@gmail.com>
2023-09-19 06:33:07 -0500

    R/L 0.0.0.11

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => { println!("You win!"); break; }
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
 
         }
     }
