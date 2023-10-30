5dea8b2d7a7a8ba58f308c47c995cee7e11147a4
KatCote <filtermr@gmail.com>
2023-10-30 04:54:44 +0300

    R/L 17

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-#![allow(unused)]
 fn main() {
-use std::arch::asm;
 
-let mut a: u64 = 4;
-let b: u64 = 4;
-let c: u64 = 4;
-unsafe {
-    asm!(
-        "add {0}, {1}",
-        "add {0}, {2}",
-        inout(reg) a,
-        in(reg) b,
-        in(reg) c,
-    );
-}
-assert_eq!(a, 12);
-println!("{}", a);
+    use std::arch::asm;
+
+    let mut port: u8 = 255;
+    let mut result: u8 = 1;
+
+    println!("{} {}", &result, &port);
+
+    unsafe{
+        asm!(
+            "add al, bl;",
+            inout("al") result,
+            inout("bl") port,
+        );
+    }
+
+    println!("{} {}", &result, &port);
 }
